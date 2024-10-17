c402dd8ab0615d72af5f0328f7d26d482133aea0
emjay026 <mojalefa.emjay01@gmail.com>
2024-10-17 23:21:14 +0200

    Do task 6, project 0x00. MySQL advanced

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,27 @@
+DELIMITER $$
+
+CREATE PROCEDURE AddBonus(
+    IN user_id INT,
+    IN project_name VARCHAR(255),
+    IN score INT
+)
+BEGIN
+    DECLARE project_id INT;
+
+    -- Check if the project already exists
+    SELECT id INTO project_id
+    FROM projects
+    WHERE name = project_name;
+
+    -- If project doesn't exist, create it
+    IF project_id IS NULL THEN
+        INSERT INTO projects (name) VALUES (project_name);
+        SET project_id = LAST_INSERT_ID();  -- Get the ID of the newly created project
+    END IF;
+
+    -- Insert the score into corrections table
+    INSERT INTO corrections (user_id, project_id, score)
+    VALUES (user_id, project_id, score);
+END$$
+
+DELIMITER ;
