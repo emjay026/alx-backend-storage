473865085ac78927389bb94717c0290e5725e6c9
emjay026 <mojalefa.emjay01@gmail.com>
2024-10-17 23:05:53 +0200

    Do task 4, project 0x00. MySQL advanced

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -6,8 +6,8 @@ AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
     UPDATE items
-    SET quantity = quantity - NEW.order_quantity         -- Decrease quantity by the ordered amount
-    WHERE item_id = NEW.item_id;                          -- Match the item_id in orders
+    SET quantity = quantity - NEW.number               -- Decrease quantity by the ordered number
+    WHERE name = NEW.item_name;                        -- Match the item_name in orders
 END$$
 
 DELIMITER ;
