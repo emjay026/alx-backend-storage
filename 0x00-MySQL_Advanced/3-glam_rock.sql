72997092a898255fa26910360480428f382fa2b5
emjay026 <mojalefa.emjay01@gmail.com>
2024-10-17 22:49:47 +0200

    Do task 3, project 0x00. MySQL advanced

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,12 +1,13 @@
 -- Query to list bands with any style containing 'glam', ranked by their longevity
 SELECT 
     band_name,
-    formed,
-    split,
-    style
+    CASE 
+        WHEN split IS NOT NULL THEN split - formed                     -- If the band split, calculate lifespan until split year
+        ELSE 2022 - formed                                              -- If the band hasn't split, calculate lifespan until 2022
+    END AS lifespan
 FROM 
     metal_bands
 WHERE 
-    style LIKE '%glam%'                                          -- Filter for bands where style contains 'glam'
+    style LIKE '%Glam rock%'                                          -- Filter for bands where style contains 'glam'
 ORDER BY 
     band_name DESC;                                             -- Order by band_name in descending order
