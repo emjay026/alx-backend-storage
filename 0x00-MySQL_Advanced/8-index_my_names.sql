0d92ec750fabe457e4cbb198b59f40917dd2b765
emjay026 <mojalefa.emjay01@gmail.com>
2024-10-18 04:56:32 +0200

    Do task 8, project 0x00. MySQL advanced

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,2 +1,6 @@
+-- Add a generated column for the first letter of the name
+ALTER TABLE names
+ADD first_letter CHAR(1) GENERATED ALWAYS AS (LEFT(name, 1)) STORED;
+
 -- Create an index on both the name and first_letter columns
 CREATE INDEX idx_name_first ON names(name, first_letter);
